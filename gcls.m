fbe13435929ddb14a2ebd4e3e9f5e71361a9a9d2
Monika Jaskolka <bialy.monika@gmail.com>
2018-04-20 14:56:09 +0000

    Fixed output comment. Formatted code.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@12565 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- gcls.m
+++ gcls.m
@@ -1,12 +1,17 @@
 function sels = gcls
-%% GCLS Get all currently selected lines.
+% GCLS Get all currently selected lines.
 %
-% 	Inputs:
-% 		N/A
+%   Inputs:
+%       N/A
 %
-% 	Outputs:
-%		sels   Cell array of line handles.
+%   Outputs:
+%       sels   Numeric array of line handles.
+%
+%   Example:
+%       lines = gcls
 
-    objs = find_system(gcs,'LookUnderMasks','on','Findall','on','FollowLinks','on','Type','line','Selected','on');
-    sels = flipud(objs);    % Flip to put in correct order (top to bottom position in model)
+    sels = find_system(gcs, 'LookUnderMasks', 'on', 'Findall', 'on', ...
+        'FollowLinks', 'on', 'Type', 'line', 'Selected', 'on');
+    % Flip order. find_system returns in descending order.
+    sels = flipud(sels);
 end
