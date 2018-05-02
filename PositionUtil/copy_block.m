2c2de93d9f4f184dcf820093bf18075b7dff6b39
Gordon Marks <gor.marks@gmail.com>
2018-05-02 15:47:56 +0000

    Added copy_block function and edited move_block.m to use it.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@12665 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- PositionUtil/copy_block.m
+++ PositionUtil/copy_block.m
@@ -1,8 +1,10 @@
-function handle = move_block(block, sys)
-    %   MOVE_BLOCK Moves a given Simulink block to a given Simulink system.
-    %       Lines that were connected to the original block will be
-    %       disconnected and the ports of the final block will have no
-    %       signals connected to it.
+function handle = copy_block(block, sys)
+    %   COPY_BLOCK Copies a given Simulink block to a given Simulink system.
+    %       The ports of the copied block will have no signals connected to
+    %       it. This is different from 
+    %           add_block(block, <newBlockName>, 'MakeNameUnique', 'On')
+    %       because <newBlockName> may cause errors or result in bugs if it
+    %       is based on an arbitrary starting block name.
     %
     %   Inputs:
     %       block   Full path or handle of a block.
@@ -12,7 +14,7 @@ function handle = move_block(block, sys)
     %       handle  The handle of the resulting block.
     %
     %   Example:
-    %       h = move_block(gcbh, get_param(bdroot(gcbh), 'Name'));
+    %       h = copy_block(gcbh, get_param(bdroot(gcbh), 'Name'));
 
     % Future work:
     % - allow user to pass additional arguments to change the
@@ -24,10 +26,20 @@ function handle = move_block(block, sys)
     % blocks in the system, in the center of all of the blocks in the
     % system, etc.
     
-    handle = copy_block(block, sys);
+    % Choose arbitrary start name for the block after moving it.
+    % If the original block name is used in add_block, then there may
+    % (will?) be undesired behaviour if the block's name parameter has a
+    % '/' character in it.
+    tmpDst = [sys '/temp_name'];
     
-    % Delete block to finish the "move"
-    delete_block(block)
+    % Add the block to the desired system.
+    % MakeNameUnique is on to avoid errors for a block named temp_name
+    % already existing.
+    handle = add_block(block, tmpDst, 'MakeNameUnique', 'On');
+    
+    % Set the name to the same as the original block (or with an appended
+    % integer to be a unique name in the system).
+    set_name_unique(handle, get_param(block, 'Name'));
 end
 
 function set_name_unique(h, baseName, varargin)
