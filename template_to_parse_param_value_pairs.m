1f9569ba4394c26030c327b52429f2635e0b5bed
Gordon Marks <gor.marks@gmail.com>
2018-06-07 07:41:46 +0000

    File with a template for adding comments about and parsing parameter-value pairs given as input through varargin.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@12971 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ template_to_parse_param_value_pairs.m
@@ -0,0 +1,34 @@
+function [param1,param2,param3] = template_to_parse_param_value_pairs(varargin)
+    % Input:
+    %   varargin	Parameter-Value pairs as detailed below.
+    %
+    % Parameter-Value pairs:
+    %   Parameter: 'Param1'
+    %   Value:  {'Default'} - (Default) Chooses default option.
+    %           {'Option2'} - Chooses the 2nd option.
+	%	Parameter: 'Param2'
+    %   Value: Any number. Default: 1.
+    %   Parameter: 'Param3'
+	%	Value:  Some cell array. Default: {1, 'default'}.
+    
+    % Handle parameter-value pairs
+    param1 = lower('Default');
+    param2 = 1;
+    param3 = {1, 'default'};
+    for i = 1:2:length(varargin)
+        param = lower(varargin{i});
+        value = lower(varargin{i+1});
+        
+        switch param
+            case 'param1'
+                assert(any(strcmp(value,{'Default','Option2'})), ['Unexpected value for ' param ' parameter.'])
+                param1 = value;
+            case 'param2'
+                param2 = value;
+            case 'param3'
+                param3 = value;
+            otherwise
+                error('Invalid parameter.')
+        end
+    end
+end
