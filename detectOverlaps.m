43132a9ef2f48c82ca96a85cba69545cca5f7662
Gordon Marks <gor.marks@gmail.com>
2018-06-08 09:17:18 +0000

    Function for detecting overlaps between blocks. Header comments could still use some work.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@12995 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ detectOverlaps.m
@@ -0,0 +1,87 @@
+function overlaps = detectOverlaps(baseBlock, otherBlocks, varargin)
+    % TODO fix header comments
+    % DETECTOVERLAPS
+    %
+    % Inputs:
+    %   baseBlock   Simulink block. We're checking if any other block
+    %               overlaps this.
+    %   otherBlocks Cell array of Simulink blocks.
+    %   varargin	Parameter-Value pairs as detailed below.
+    %
+    % Parameter-Value pairs:
+    %   Parameter: 'OverlapType'
+    %   Value:  {'Vertical'} - Detects any overlap with respect to top and
+    %               bottom positions (i.e. blocks could be offset on the
+    %               x-axis, but still be deemed overlapping).
+    %           {'Horizontal'} - Detects any overlap with respect to left
+    %               and right positions (i.e. blocks could be offset on the
+    %               y-axis, but still be deemed overlapping).
+    %           {'Any'} - Detects blocks with either a vertical or
+    %               horizontal overlap.
+    %           {'All'} - (Default) Detects blocks sharing space.
+    %
+    % Outputs:
+    %   overlaps    Cell array of Simulink blocks in otherBlocks that
+    %               overlap baseBlock.
+    %
+    
+    % Handle parameter-value pairs
+    OverlapType = lower('All');
+    for i = 1:2:length(varargin)
+        param = lower(varargin{i});
+        value = lower(varargin{i+1});
+        
+        switch param
+            case lower('OverlapType')
+                assert(any(strcmp(value,lower({'Vertical','Horizontal','Any','All'}))), ...
+                    ['Unexpected value for ' param ' parameter.'])
+                OverlapType = value;
+            otherwise
+                error('Invalid parameter.')
+        end
+    end
+    
+    overlaps = cell(1,length(otherBlocks));
+    for i = 1:length(otherBlocks)
+        switch OverlapType
+            case 'Vertical'
+                % Detect vertical overlaps
+                overlapFound = isOverlap(baseBlock,otherBlocks{i},[2,4]); % Check for vertical overlap
+            case 'Horizontal'
+                % Detect horizontal overlaps
+                overlapFound = isOverlap(baseBlock,otherBlocks{i},[1,3]); % Check for vertical overlap
+            case 'Any'
+                % Detect vertical or horizontal overlaps
+                overlapFound = isOverlap(baseBlock,otherBlocks{i},[2,4]) ...
+                    || isOverlap(baseBlock,otherBlocks{i},[1,3]);
+            case 'All'
+                % Detect vertical and horizontal overlaps (i.e. both
+                % occurring at once)
+                overlapFound = isOverlap(baseBlock,otherBlocks{i},[2,4]) ...
+                    && isOverlap(baseBlock,otherBlocks{i},[1,3]);
+            otherwise
+                error('Unexpected paramter.')
+        end
+        if overlapFound
+            overlaps{i} = otherBlocks{i};
+        end
+    end
+    overlaps(cellfun('isempty',overlaps)) = []; % Empty elements are non-matches and should be removed
+end
+
+function bool = isOverlap(block1, block2, dims)
+    %
+    % dims = [2,4] checks for vertical overlap
+    % dims = [1,3] checks for horizontal overlap
+    pos1 = get_param(block1, 'Position');
+    pos2 = get_param(block2, 'Position');
+    
+    bool = isRangeOverlap(pos1(dims),pos2(dims));
+end
+
+function bool = isRangeOverlap(range1,range2)
+    assert(range1(1)<range1(2))
+    assert(range2(1)<range2(2))
+    
+    bool = range1(1)<=range2(2) && range2(1)<=range1(2);
+end
