52ae4ea68208cc4b479d1629473ed19c7afe850c
Gordon Marks <gor.marks@gmail.com>
2018-06-08 07:59:49 +0000

    Moved setOrientations.m to utilities.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@12994 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ PositionUtil/setOrientations.m
@@ -0,0 +1,25 @@
+function setOrientations(blocks,varargin)
+    % SETORIENTATIONS Orient blocks. Default puts outputs on right.
+    %
+    %   Inputs:
+    %       blocks      Cell array of blocks to use.
+    %       varargin    Character array indicating a direction for a block's
+    %                   outports to face (if block has no outputs, it's the
+    %                   direction they would face). Options correspond with the
+    %                   'Orientation' block property e.g. 'right' (default),
+    %                   'left', 'up', 'down'.
+    %
+    %   Example:
+    %       setOrientations(gcbs)
+    %       setOrientations(gcbs, 'left')
+    
+    if nargin > 1
+        direction = varargin{1};
+    else
+        direction = 'right';
+    end % Ignore inputs beyond the second
+    
+    for i = 1:length(blocks)
+        set_param(blocks{i}, 'Orientation', direction);
+    end
+end
