efe3716c89a36d5e4edfd5f2a0320ada923ecf81
Gordon Marks <gor.marks@gmail.com>
2018-06-09 05:09:55 +0000

    Second version for aligning based on inports.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@13016 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- PositionUtil/Alignment/alignIn.m
+++ PositionUtil/Alignment/alignIn.m
@@ -1,8 +1,23 @@
 function alignIn(blocks)
-    % First version
-    for i = 1:length(blocks)
-        in = getPorts(blocks{i}, 'Inport'); in = in(1);
-        out = getSrcs(in); out = out{1};
-        alignPorts(out,in)
+    % Reposition each block to align its first inport with a port it
+    % connects to.
+    % Aligns from the first block to the last block in blocks.
+    %
+    
+    % TODO: add support for round sum blocks
+    % TODO: choose inport for output-input pairs more smartly
+    %   e.g. don't choose one if it's not facing
+    %   e.g. if multiple are facing, then which is closer
+    
+    % Second version
+    for o = 1:length(blocks)
+        in = getPorts(blocks{o}, 'Inport');
+        if ~isempty(in)
+            in = in(1);
+            out = getSrcs(in);
+            out = out{1};
+            
+            alignPorts(out,in)
+        end
     end
 end
