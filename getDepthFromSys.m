e25b52d2661b7aa97a4708bf7dcc4d4a1ed359b8
Gordon Marks <gor.marks@gmail.com>
2018-06-19 08:13:32 +0000

    Added function to get depth of one system relative to another.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@13121 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ getDepthFromSys.m
@@ -0,0 +1,12 @@
+function depth = getDepthFromSys(sys, sys2)
+    % Get depth of sys2 from sys
+    % Return -1 if sys2 is not in sys
+    
+    if strcmp(sys,sys2)
+        depth = 0;
+    elseif strcmp(bdroot(sys2),sys2)
+        depth = -1;
+    else
+        depth = getDepthFromSys(sys, get_param(sys2, 'Parent'));
+    end
+end
