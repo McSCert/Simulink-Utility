3a738eebb5f0698a0087efe0bd929205c2d97f58
Gordon Marks <gor.marks@gmail.com>
2018-08-13 03:20:05 +0000

    Edited to use a better approach. Old approaches are in separate functions in the file in case they are desired in the future.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@13620 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- AinB.m
+++ AinB.m
@@ -1,22 +1,31 @@
 function bool = AinB(A,B)
-% AINB Determine if string A is an element in cell array B.
-%
-%   Inputs:
-%       A       Character vector.
-%       B       Cell array.
-%
-%   Outputs:
-%       bool    Whether A is in B (1), or not(0).
-%
-%   Examples:
-%       AinB('a',{'a','b','c'}) -> true
-%       AinB('a',{'abc'})       -> false
-%       AinB({'a'},{{'a'}})     -> bad input results not guaranteed
-%       AinB('a',{{'a'}})       -> false
-%
-%   Another method to do this using built-in functions:
-%       ~isempty(find(strcmp(A,B)))
+    % AINB Determine if string A is an element in cell array B. Please just
+    % call any(strcmp(A,B)) instead of using this function.
+    %
+    %   Inputs:
+    %       A       Character vector.
+    %       B       Cell array.
+    %
+    %   Outputs:
+    %       bool    Whether A is in B (1), or not(0).
+    %
+    %   Examples:
+    %       AinB('a',{'a','b','c'}) -> true
+    %       AinB('a',{'abc'})       -> false
+    %       AinB({'a'},{{'a'}})     -> bad input results not guaranteed
+    %       AinB('a',{{'a'}})       -> false
+    %
+    
+    bool = any(strcmp(A,B));
+end
 
+function bool = AinB_1(A,B)
+    %   Another method to do this using built-in functions:
+    %       ~isempty(find(strcmp(A,B)))
+    bool = ~isempty(find(strcmp(A,B)));
+end
+
+function bool = AinB_2(A,B)
     bool = false;
     if ischar(A) && iscell(B)
         for i = 1:length(B)
