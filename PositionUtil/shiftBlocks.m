0d969c435d1376dbda9cc85e98bdde6eb68c05be
Gordon Marks <gor.marks@gmail.com>
2018-08-27 15:07:41 +0000

    improved comments.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@13794 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- PositionUtil/shiftBlocks.m
+++ PositionUtil/shiftBlocks.m
@@ -1,10 +1,15 @@
 function shiftBlocks(blocks, shift)
     %
-    % blocks - Cell array of blocks
-    % shift - 1x4 vector to add to position value of blocks
+    %   block       List (cell array or vector) of Simulink blocks (fullnames or
+    %               handles).
+    %   shift       1x4 vector to add to position value of blocks.
     
+    %
+    blocks = inputToNumeric(blocks);
+    
+    %
     for i = 1:length(blocks)
-        b = blocks{i};
+        b = blocks(i);
         pos = get_param(b, 'Position');
         set_param(b, 'Position', pos + shift);
     end
