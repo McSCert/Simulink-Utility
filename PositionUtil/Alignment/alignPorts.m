a7cf9a29a3a46e1efe70edd9d6c3669029e619c5
Gordon Marks <gor.marks@gmail.com>
2018-08-27 15:09:29 +0000

    Added output of the block's new position and added option to just get that without actually moving it.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@13796 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- PositionUtil/Alignment/alignPorts.m
+++ PositionUtil/Alignment/alignPorts.m
@@ -1,9 +1,20 @@
-function alignPorts(port1, port2)
+function position = alignPorts(port1, port2, varargin)
 % ALIGNPORTS Move the second block veritcally so that the ports are aligned.
+% Return position of the moved block.
 
+    % Option to not perform the operation, but just return the position that
+    % the second block would have been moved to.
+    if isempty(varargin)
+        performAlign = true;
+    else
+        performAlign = varargin{1};
+    end
+
+    %
     assert(strcmp(get_param(port1, 'Type'), 'port') && (strcmp(get_param(port2, 'Type'), 'port')), ...
         'Inputs port1 and port2 must be ports.');
 
+    %
     port2Block = get_param(port2, 'Parent');
     port2BlockPos = get_param(port2Block, 'Position');
     
@@ -15,5 +26,8 @@ function alignPorts(port1, port2)
     
     newTop = port2BlockPos(2) - portDistanceY;
     newBottom = port2BlockPos(2) + port2BlockHeight - portDistanceY;
-    set_param(port2Block, 'Position', [port2BlockPos(1), newTop, port2BlockPos(3), newBottom]);
+    position = [port2BlockPos(1), newTop, port2BlockPos(3), newBottom];
+    if performAlign
+        set_param(port2Block, 'Position', position)
+    end % else return position
 end
