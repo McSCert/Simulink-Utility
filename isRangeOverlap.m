36241f44bc8aec63c1dc27e683f1647abcc03b27
Gordon Marks <gor.marks@gmail.com>
2018-08-28 05:34:09 +0000

    Editted header comments.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@13823 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- isRangeOverlap.m
+++ isRangeOverlap.m
@@ -1,6 +1,6 @@
 function bool = isRangeOverlap(range1,range2)
-    % ISRANGEOVERLAP Detect if there is any common value between 2 ranges of
-    % values.
+    % ISRANGEOVERLAP Detect whether or not the union of two ranges have and
+    % intersection.
     %
     % Input:
     %   range1  1x2 vector given with the lower value first.
