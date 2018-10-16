170e764b40f681214bb67c17437b0c95f960afd0
Monika Jaskolka <bialy.monika@gmail.com>
2018-10-16 00:42:07 +0000

    Edited comment.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@14355 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- PositionUtil/moveBelow.m
+++ PositionUtil/moveBelow.m
@@ -1,15 +1,18 @@
-function moveBelow(topBlock,botBlock,buffer)
-    %
-    %
-    % Inputs:
-    %   topBlock    Reference block.
-    %   botBlock    Move this block below the reference.
-    %   buffer      Amount below the reference to move botBlock.
-    %
-    
+function moveBelow(topBlock, botBlock, buffer)
+% MOVEBELOW Move a block to be below another block.
+%
+%   Inputs:
+%       topBlock    Reference block.
+%       botBlock    Move this block below the reference.
+%       buffer      Amount below the reference to move botBlock.
+%
+%   Outputs:
+%       N/A
+
+
     pos1 = get_param(topBlock, 'Position');
     pos2 = get_param(botBlock, 'Position');
-    
+
     shift = (pos1(4)+buffer-pos2(2))*[0 1 0 1]; % [0 1 0 1] only shifts top and bottom
     set_param(botBlock, 'Position', pos2 + shift)
 end
