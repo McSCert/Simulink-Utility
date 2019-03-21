85fc05b58c6a7befbe62dd7a194b5449a732f1be
Gordon Marks <gor.marks@gmail.com>
2019-03-21 18:17:51 +0000

    Sometimes new_system errors, but still produces the model (and it's not obvious because it isn't opened) so I made new_system_makenameunique.m just issue a warning and indicated that the model was still created.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@16125 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- new_system_makenameunique.m
+++ new_system_makenameunique.m
@@ -1,18 +1,30 @@
 function h = new_system_makenameunique(baseName, varargin)
-% NEW_SYSTEM_MAKENAMEUNIQUE new_system command except that it appends a
-%   number to the name to ensure a file with the name does not exist.
-%
-%   For more information about new_system, type: "help new_system" at the
-%   command line.
-
+    % NEW_SYSTEM_MAKENAMEUNIQUE new_system command except that it appends a
+    %   number to the name to ensure a file with the name does not exist.
+    %
+    %   For more information about new_system, type: "help new_system" at the
+    %   command line.
+    
     name = baseName;
-    if exist(name, 'file') == 4
+    if exist(name, 'file') == 4 || bdIsLoaded(name)
         n = 1;
-        while exist(strcat(name, num2str(n)), 'file') == 4
+        while exist(strcat(name, num2str(n)), 'file') == 4 ...
+                || bdIsLoaded(strcat(name, num2str(n)))
             n = n + 1;
         end
         name = strcat(name, num2str(n));
     end
-
-    h = new_system(name,varargin{:});
+    
+    try
+        h = new_system(name,varargin{:});
+    catch ME
+        if bdIsLoaded(name)
+            warning(['Error occurred in ' 'new_system' '. ' ...
+                'Block diagram ' name ' was created. ' ...
+                'The error message follows:' char(10) getReport(ME)])
+            h = get_param(name, 'Handle');
+        else
+            rethrow(ME)
+        end
+    end
 end
