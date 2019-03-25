4b850ceda1c9072eefd906acf68624de39e5d9a7
Monika Jaskolka <bialy.monika@gmail.com>
2019-03-25 21:11:23 +0000

    Changed gcls to include currently selected commented lines.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@16138 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- GetCurrentFunctions/gcls.m
+++ GetCurrentFunctions/gcls.m
@@ -8,10 +8,21 @@ function sels = gcls
     %       sels   Numeric array of line handles.
     %
     %   Example:
-    %       lines = gcls
+    %       >> lines = gcls
+    %
+    %   lines =
+    %       26.0001
+    %       28.0004
     
-    objs = find_system(gcs, 'LookUnderMasks', 'on', 'Findall', 'on', ...
-        'FollowLinks', 'on', 'Type', 'line', 'Selected', 'on');
+    
+    if verLessThan('simulink', '8.2') % IncludeCommented available in 2013b (8.2) and higher
+        objs = find_system(gcs, 'LookUnderMasks', 'on', 'Findall', 'on', ...
+            'FollowLinks', 'on', 'Type', 'line', 'Selected', 'on');
+    else
+        objs = find_system(gcs, 'LookUnderMasks', 'on', 'Findall', 'on', ...
+            'FollowLinks', 'on', 'Type', 'line', 'Selected', 'on', 'IncludeCommented', 'on');
+    end
+    
     % Flip order. find_system returns in descending order.
     sels = flipud(objs);
 end
