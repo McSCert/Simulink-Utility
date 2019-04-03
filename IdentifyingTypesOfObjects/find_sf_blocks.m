007357ffbe816ed009b114e8dcc9bea7aad7a157
Gordon Marks <gor.marks@gmail.com>
2019-04-03 20:46:33 +0000

    Added function to find all blocks in a system with SFBlockType other than 'NONE'.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@16221 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ IdentifyingTypesOfObjects/find_sf_blocks.m
@@ -0,0 +1,28 @@
+function [blocks, types] = find_sf_blocks(sys)
+    % FIND_SF_BLOCKS Find blocks in sys with 'SFBlockType' parameter that is not 'NONE'.
+    %
+    % Inputs:
+    %   sys     Simulink system.
+    %
+    % Outputs:
+    %   blocks  nx1 cell array of block names.
+    %   types   nx1 cell array of the corresponding block's 'SFBlockType'.
+    %
+    
+    blocks = {};
+    types = {};
+    
+    sysBlocks = find_system(sys, 'Type', 'Block');
+    for i = 1:length(sysBlocks)
+        b = sysBlocks{i};
+        if strcmp(get_param(b, 'BlockType'), 'SubSystem')
+            type = get_param(b, 'SFBlockType');
+            if ~strcmp(type, 'NONE')
+                blocks{end+1} = b;
+                types{end+1} = type;
+            end
+        end
+    end
+    blocks = blocks';
+    types = types';
+end
