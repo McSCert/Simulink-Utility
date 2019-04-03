1351eb34115eaa03f907492135da677887d6505e
Gordon Marks <gor.marks@gmail.com>
2019-04-03 20:14:20 +0000

    Added an alternate approach that might be better, but that I haven't tested.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@16218 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- isaMatlabFunctionBlock.m
+++ isaMatlabFunctionBlock.m
@@ -2,3 +2,10 @@ function bool = isaMatlabFunctionBlock(block)
     obj = find(slroot, '-isa', 'Stateflow.EMChart', 'Path', getfullname(block));
     bool = ~isempty(obj);
 end
+
+function bool = isaMatlabFunctionBlock_alternate(block)
+    % This approach is untested
+    if strcmp(get_param(block, 'BlockType'), 'SubSystem')
+        bool = strcmp(get_param(block, 'SFBlockType'), 'MATLAB Function');
+    end
+end
