db82f086ff985dfd00205f88ad794410dfe61ecb
Gordon Marks <gor.marks@gmail.com>
2019-04-09 18:38:15 +0000

    Changed inport and outport functions to look for top-level ones.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@16297 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- IdentifyingTypesOfObjects/find_outports.m
+++ IdentifyingTypesOfObjects/find_outports.m
@@ -1,5 +1,5 @@
 function [numOutports, outports] = find_outports(sys)
-    outports = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'Outport');
+    outports = find_system(sys, 'SearchDepth', '1', 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'Outport');
    
     numOutports = length(outports);
 end
