f47c58182852e9dca50fb0607c9bd132e1fa2017
Gordon Marks <gor.marks@gmail.com>
2019-07-02 22:31:04 +0000

    Updated getPorts so the 2nd input is optional and to allow additional inputs to indicate required parameter values (in the same way find_system has required values for the objects it returns).
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@17594 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- DataflowTracing/getPorts.m
+++ DataflowTracing/getPorts.m
@@ -1,29 +1,57 @@
-function ports = getPorts(blk, type)
-% GETPORTS Get the ports, meeting a type constraint (see below), for a block.
-%
-%   Inputs:
-%       blk     Fullname or handle of a block.
-%       type    Char array indicating the type of port.
-%               Any single port type is accepted (case sensitive). The
-%               following are also accepted (case insensitive):
-%               'All' indicates all types.
-%               'In' indicates all incoming ports (everything except Outports).
-%               'Out' indicates all outgoing ports (Outports).
-%               'Basic' indicates Inports and Outports.
-%               'Special' indicates all ports other than Inports and Outports.
-%
-%   Outputs:
-%       ports   List of handles.
-
+function ports = getPorts(blk, type, varargin)
+    % GETPORTS Get the ports, meeting a type constraint (see below), for a
+    % block.
+    %
+    %   Inputs:
+    %       blk         Fullname or handle of a block.
+    %       type        Char array indicating the type of port.
+    %                   Any single port type is accepted (case sensitive). The
+    %                   following are also accepted (case insensitive):
+    %                   'All' indicates all types.
+    %                   'In' indicates all incoming ports (everything except
+    %                       Outports).
+    %                   'Out' indicates all outgoing ports (Outports).
+    %                   'Basic' indicates Inports and Outports.
+    %                   'Special' indicates all ports other than Inports and
+    %                       Outports.
+    %                   Default: 'All'
+    %       varargin    Parameter-value pairs where only the ports which have
+    %                   the specified values for the corresponding parameters
+    %                   will be returned.
+    %
+    %   Outputs:
+    %       ports   List of handles.
+    %
+    %   Examples:
+    %       % Get all ports of the selected block:
+    %       getPorts(gcb)
+    %       % or:
+    %       getPorts(gcb, 'All')
+    %       % Get all outports of the selected block:
+    %       getPorts(gcb, 'Out')
+    %       % or:
+    %       getPorts(gcb, 'outport')
+    %       % or:
+    %       getPorts(gcb, 'All', 'PortType', 'outport')
+    %       % Get the inport with port number of 2:
+    %       getPorts(gcb, 'inport', 'PortNumber', 2)
+    %
+    
+    % Input handling:
+    if nargin == 1
+        type = 'all';
+    end
+    
+    % 
     ph = get_param(blk, 'PortHandles');
     pfields = fieldnames(ph);
-
+    
     typei = lower(type); % lowercase to make it case insensitive
     switch typei
         case 'all'
             indices = 1:length(pfields); % for all field types
         case 'in'
-            indices = find(~strcmp('Outport',pfields)); % for all inport field types
+            indices = find(~strcmp('Outport',pfields)); % for all input field types
         case 'out'
             indices = find( strcmp('Outport',pfields)); % for Outport field type
         case 'basic'
@@ -37,5 +65,33 @@ function ports = getPorts(blk, type)
     for i = 1:length(indices)
         idx = indices(i);
         ports = [ports, ph.(pfields{idx})];
+    end
+    
+    % Remove ports that don't match the parameter-value pair requirements.
+    for i = 1:2:length(varargin)
+        param = varargin{i};
+        desiredValue = varargin{i+1};
+        
+        giveWarning = false;
+        for j = length(ports):-1:1
+            % Remove ports that have a different value for the same parameter or
+            % if they don't even have the parameter.
+            keepPort = false;
+            try
+                value = get_param(ports(j), param);
+                if isequal(value, desiredValue)
+                    keepPort = true;
+                end
+            catch
+                giveWarning = true;
+            end
+            if ~keepPort
+                ports(j) = [];
+            end
+        end
+        if giveWarning
+            warning(['Not all candidate ports could be compared using the parameter ', param, '.', char(10), ...
+                'Ports that gave an error for that parameter are assumed not to be desired results for ' mfilename '.'])
+        end
     end
 end
