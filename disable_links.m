5532532ae1476bb451a5fc2885f2fe9282f00f5c
Gordon Marks <gor.marks@gmail.com>
2019-11-20 18:18:50 +0000

    Function for disabling all links in a model - returns subsystems whose links are not none as these may still need to be broken in some way.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@19186 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ disable_links.m
@@ -0,0 +1,23 @@
+function linkedSubs = disable_links(sys)
+    % sys           System to disable links within.
+    % linkedSubs    Cell array of subsystems which still have a LinkStatus other
+    %               than none.
+    
+    subs = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', ...
+        'Variants', 'AllVariants', 'IncludeCommented', 'on', ...
+        'BlockType', 'SubSystem');%, 'LinkStatus', 'resolved');
+    
+    for i = 1:length(subs)
+        if strcmp(get_param(subs{i}, 'LinkStatus'), 'resolved')
+            set_param(subs{i}, 'LinkStatus', 'none');
+        end
+    end
+    
+    linkedSubs = {};
+    for i = 1:length(subs)
+        ls = get_param(subs{i}, 'LinkStatus');
+        if ~strcmp(ls, 'none')
+            linkedSubs{end+1} = subs{i};
+        end
+    end
+end
