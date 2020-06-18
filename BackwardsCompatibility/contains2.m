1e4b00b2204561c5ba7a3d248ae51cb8a1717106
Monika Jaskolka <bialy.monika@gmail.com>
2020-06-18 14:16:50 +0000

    Updateing contains2.m so that it works with cell arrays.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/Utility@20648 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- BackwardsCompatibility/contains2.m
+++ BackwardsCompatibility/contains2.m
@@ -2,9 +2,23 @@ function b = contains2(str, pattern)
 % CONTAINS True if pattern is found in text.
 %   TF = CONTAINS(STR,PATTERN) returns 1 (true) if STR contains PATTERN,
 %   and returns 0 (false) otherwise.
+%
+%   STR can be a character array or a cell array of characters.
 
-    b = cell2mat(strfind(str, pattern));
-    if isempty(b)
-        b = false;
+    b = strfind(str, pattern);
+    if iscell(str)
+        for i = 1:length(b)
+            if isempty(b{i})
+                b{i} = 0;
+            end
+        end
+        b = logical(cell2mat(b));
+    elseif ischar(str)
+        b = find(b);
+        if b > 0
+            b = true;
+        else
+            b = false;
+        end
     end
 end
